bf015a75313cbecafe61a41644deb6d3080bd2f7
Christopher <easonmu@hotmail.com>
2022-12-29 23:48:41 +0800

    feat: test: add wx api

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,14 +1,21 @@
 // index.ts
 // 获取应用实例
-const app = getApp<IAppOption>()
+const app = getApp<IAppOption>();
 
 Page({
   data: {
-    src: "http://localhost:8080/#/"
+    src: "http://localhost:8080/#/",
   },
   // 事件处理函数
-  bindViewTap() {
+  onMessage(event: any): void {
+    console.log(event, "[event]");
+    wx.openLocation({ longitude: 121.6, latitude: 24.9 });
   },
-  onLoad() {
+  onLoad(): void {},
+  onShareAppMessage(): any {
+    return {
+      title: "xx",
+      path: "pages/index",
+    };
   },
-})
+});
